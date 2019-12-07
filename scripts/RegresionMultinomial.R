2bbedc3ec87616c0e09f64e8c775988787c46249
zyntonyson <zyntonyson@.gmail.com>
2019-12-07 00:22:17 -0600

    #Agregar regresion multinomial

---
+++ scripts/RegresionMultinomial.R
@@ -0,0 +1,9 @@
+# Referencias
+# https://stackoverflow.com/questions/51843592/glmnet-multinomial-logistic-regression-prediction-result
+# https://bookdown.org/egarpor/PM-UC3M/app-ext-multinomialreg.html
+# https://bookdown.org/egarpor/PM-UC3M/glm-model.html#glm-model-log
+# http://www.sthda.com/english/articles/36-classification-methods-essentials/149-penalized-logistic-regression-essentials-in-r-ridge-lasso-and-elastic-net/
+# https://stats.stackexchange.com/questions/305333/how-to-fit-multinomial-logistic-regression-using-lasso-into-k-1-in-r
+# https://rpubs.com/heruwiryanto/multinom-reg
+# https://www.institutomora.edu.mx/testU/SitePages/martinpaladino/modelos_logit_con_R.html#modelos-ligit-multinomiales.
+# https://stats.idre.ucla.edu/r/dae/multinomial-logistic-regression/

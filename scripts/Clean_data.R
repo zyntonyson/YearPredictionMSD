59b5caeed8008af3e01c389461bf4402db5b3d97
zyntonyson <zyntonyson@.gmail.com>
2019-12-08 22:49:51 -0600

    #Actualizaciones y regresion multinomial

--- scripts/Clean_data.R
+++ scripts/Clean_data.R
@@ -39,6 +39,8 @@ data %<>%
   mutate(decades=sapply(year,function(x){x-x%%10})) %>% 
   mutate(decades=sapply(decades,function(x){ifelse(x<1950,1940,x)}))
 
+
+# No pude correrlo
 v = table(data$decades)[-1]
 muestras <- lapply(1:7, function (x) sample(1:v[x], v[x]*.6))
 
@@ -46,4 +48,15 @@ muestras <- lapply(1:7, function (x) sample(1:v[x], v[x]*.6))
 data <- as.data.frame(data)
 
 data[data$decades==2010,]%>%data[muestras[[1]],]
+
+
+# Data balanceado para clasificación
+# 
+dataclasificador<-data %>% 
+  mutate(decades=sapply(year,function(x){x-x%%10})) %>% 
+  mutate(decades=sapply(decades,function(x){ifelse(x<1950,1940,x)}))
+
+
+
+
 
